b44f83eae0ab0a726db8a569b6c8b864bb892ad1
Shreyash68 <yashgadekar29@gmail.com>
2026-01-01 01:39:26 +0530

    added auth and RBAC

--- backend/app/utils/rbac.py
+++ backend/app/utils/rbac.py
@@ -1,2 +1,10 @@
-# RBAC utilities
-pass
+from fastapi import Depends, HTTPException ,status
+from app.core.dependencies import get_current_user
+
+async def require_admin(current_user = Depends(get_current_user)):
+    if current_user["role"] != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Admin access required"
+        )
+    return current_user
