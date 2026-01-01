22302e3379b4b9f8f95fc79015566f944a1c5d17
Shreyash68 <yashgadekar29@gmail.com>
2026-01-01 15:58:39 +0530

    removed /api in routers (causing extra api parameter also in nginx )

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -3,7 +3,7 @@ from api.v1.auth.router import router as auth_router
 from api.v1.tasks.router import router as task_router
 from api.v1.users.router import router as user_router
 
-router = APIRouter(prefix="/api/v1")
+router = APIRouter(prefix="/v1")
 
 router.include_router(auth_router)
 router.include_router(task_router)
