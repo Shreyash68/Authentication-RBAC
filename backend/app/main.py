d1ec0465699d7c6e99282e0bb59dcc2bfa330887
Shreyash68 <yashgadekar29@gmail.com>
2026-01-01 15:53:56 +0530

    root path change for fastapi

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.v1.router import router as v1_router
 
-app = FastAPI(title="TaskFlow API", version="1.0.0")
+app = FastAPI(title="TaskFlow API", version="1.0.0",root_path="/api")
 
 app.add_middleware(
     CORSMiddleware,
